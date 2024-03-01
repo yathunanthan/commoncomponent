b3d6dcb3f59c909d4ebcd82d6e33560dab4f933c
yathunanthan <yathunanthan@commusoft.com>
2024-03-01 18:18:03 +0530

    added card

--- lib/parentpayment.module.d.ts
+++ lib/parentpayment.module.d.ts
@@ -7,8 +7,9 @@ import * as i5 from "./payment-sections/payment-applepay/payment-applepay.compon
 import * as i6 from "./payment-details/payment-details.component";
 import * as i7 from "./payment-details/payment-bank-details/payment-bank-details.component";
 import * as i8 from "./payment-details/payment-card-details/payment-card-details.component";
+import * as i9 from "@angular/common";
 export declare class ParentpaymentModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<ParentpaymentModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<ParentpaymentModule, [typeof i1.ParentpaymentComponent, typeof i2.PaymentSectionsComponent, typeof i3.PaymentCardComponent, typeof i4.PaymentBankComponent, typeof i5.PaymentApplepayComponent, typeof i6.PaymentDetailsComponent, typeof i7.PaymentBankDetailsComponent, typeof i8.PaymentCardDetailsComponent], never, [typeof i1.ParentpaymentComponent]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<ParentpaymentModule, [typeof i1.ParentpaymentComponent, typeof i2.PaymentSectionsComponent, typeof i3.PaymentCardComponent, typeof i4.PaymentBankComponent, typeof i5.PaymentApplepayComponent, typeof i6.PaymentDetailsComponent, typeof i7.PaymentBankDetailsComponent, typeof i8.PaymentCardDetailsComponent], [typeof i9.CommonModule], [typeof i1.ParentpaymentComponent]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<ParentpaymentModule>;
 }
