fdd18b61ee7e74e7de34ab917b418a9f60dbd550
yathunanthan <yathunanthan@commusoft.com>
2024-03-04 15:15:48 +0530

    work section version 0.0.10

--- lib/parentpayment.module.d.ts
+++ lib/parentpayment.module.d.ts
@@ -6,8 +6,9 @@ import * as i4 from "./payment-details/payment-card-details/payment-card-details
 import * as i5 from "@angular/common";
 import * as i6 from "@angular/common/http";
 import * as i7 from "@angular/forms";
+import * as i8 from "ngx-card";
 export declare class ParentpaymentModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<ParentpaymentModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<ParentpaymentModule, [typeof i1.ParentpaymentComponent, typeof i2.PaymentDetailsComponent, typeof i3.PaymentBankDetailsComponent, typeof i4.PaymentCardDetailsComponent], [typeof i5.CommonModule, typeof i6.HttpClientModule, typeof i7.ReactiveFormsModule, typeof i7.FormsModule], [typeof i1.ParentpaymentComponent]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<ParentpaymentModule, [typeof i1.ParentpaymentComponent, typeof i2.PaymentDetailsComponent, typeof i3.PaymentBankDetailsComponent, typeof i4.PaymentCardDetailsComponent], [typeof i5.CommonModule, typeof i6.HttpClientModule, typeof i7.ReactiveFormsModule, typeof i7.FormsModule, typeof i8.CardModule], [typeof i1.ParentpaymentComponent]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<ParentpaymentModule>;
 }
